f9123eb6ccbcc2b35c11830f06ca53ca6a27fe67
ScottBorland <scottborland0@gmail.com>
2020-02-18 16:15:37 +0000

    3D placement of planets and particles working as well as 3D physics. Function needed to remove lines

--- planet.js
+++ planet.js
@@ -1,12 +1,14 @@
 var strengthToSizeScaler = 3;
 
-function planet(x, y, strength, redrawn){
-  this.position = createVector(x, y)
+function planet(x, y, z, strength, redrawn){
+  this.position = createVector(x, y, z)
   this.strength = strength
 
   this.redrawn = redrawn
   if(!this.redrawn){
-  this.colour = color(random(255), random(255), random(255))
+  this.r = Math.floor(random(255))
+  this.g = Math.floor(random(255))
+  this.b = Math.floor(random(255))
   this.data = []
   this.data.push(this.position)
   this.data.push(this.strength)
@@ -14,11 +16,27 @@ function planet(x, y, strength, redrawn){
   planetData.push(this.data)
 }
 
+
+  var geometry = new THREE.SphereGeometry(5, 32, 32);
+  var material = new THREE.MeshBasicMaterial();
+  material.color = new THREE.Color(`rgb(${this.r},${this.g},${this.b})`)
+
+  this.sphere = new THREE.Mesh(geometry, material);
+
+  scene.add(this.sphere);
+
+
   this.display = function(){
-    stroke(0)
-    fill(this.colour)
-    ellipseMode(CENTER)
-    ellipse(this.position.x, this.position.y, this.strength * strengthToSizeScaler)
+    // stroke(0)
+    // fill(this.colour)
+    // ellipseMode(CENTER)
+    // ellipse(this.position.x, this.position.y, this.strength * strengthToSizeScaler)
+
+    this.sphere.position.x = this.position.x;
+    this.sphere.position.y = this.position.y;
+    this.sphere.position.z = this.position.z;
+    this.sphere.radius = this.strength;
+
   }
   this.applyGravity = function (){
     for(var i = 0; i < particles.length; i++){
--- sketch.js
+++ sketch.js
@@ -13,53 +13,69 @@ planets = []
 planetData = []
 particleData = []
 
+var universeSizeX = 400;
+var universeSizeY = 400;
+var universeSizeZ = 400;
+
 var analysisMode = false
 
 var spawnParticles = true
 
-//perlin noise variables
-var xr = 0.0
-var xg = 100.0
-var xb = 2000.0
+let scene, camera, renderer, sphere, controls;
 
-//arbitrary values
-var iR = 1000
-var iB = 100
-var iG = 1
+function init(){
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  renderer = new THREE.WebGLRenderer({antialias: true});
 
-//rate at which perlin noise function is looped through
-var speed = 0.02
+  var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+  scene.add( light );
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  controls = new THREE.OrbitControls( camera, renderer.domElement );
+
+  //scene.background = new THREE.Color("rgb(255, 255, 255)");
+
+  camera.position.z = 100;
+  controls.enableKeys = true;
+  controls.keys = {
+	LEFT: 37, //left arrow
+	UP: 38, // up arrow
+	RIGHT: 39, // right arrow
+	BOTTOM: 40 // down arrow
+  }
+  controls.zoomSpeed = 2.5;
+  controls.keyPanSpeed = 25;
+  controls.update();
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+init();
+animate();
+
+function animate(){
+  requestAnimationFrame(animate);
+
+  controls.update();
+
+  renderer.render(scene, camera);
+  renderer.setClearColor( 0xffffff );
+}
 
 function setup() {
-  createCanvas(1920, 1080)
-  background(255)
 
-  //worldCamera = new Camera()
+   planets.push(new planet(40, 80, 50, 10))
+   planets.push(new planet(60, 200, 100, 10))
+   planets.push(new planet(50, 400, 20, 10))
 
-  //this can be played around with, scale is between 1 and 4, fallout is from 0 to 1. (4, 1) gives a very washed out look. (2, 0.2) seems the best so far.
-  noiseDetail(2, 0.2)
-
-  // planets.push(new planet(400, 200, 10))
-  // planets.push(new planet(600, 200, 10))
-  // planets.push(new planet(500, 400, 10))
-
-  //particles.push(new particle(330, 200, 2, -7))
-  //particles.push(new particle(500, 100, 4, 6))
-
-  for(var i = 0; i < 2; i++){
-    planets.push(new planet(random(200, windowWidth - 200), random(200, windowHeight-200), random(20), false))
-  }
-  offset = createVector(0, 0)
+  particles.push(new particle(330, 200, 100, 0, 0, 0))
+  particles.push(new particle(500, 100, 4, 0, 0, 0))
 }
 
 function draw() {
-
-  if(!analysisMode){
-    //translate(width / 2, height / 2);
-    scale(0.8)
-  }else{
-    scale(0.8)
-  }
 
   for(var i = 0; i < particles.length; i++){
     if(particles[i].destroy){
@@ -82,107 +98,41 @@ function draw() {
       counter = 0
   }
   if(spawnParticles && counter % 3 == 0){
-    particles.push(new particle(random(400, windowWidth -400), random(400, windowHeight -400), random(5), random(5), false))
-  }
-
-  //perlin noise
-  var rx = xr + iR;
-  var gx = xg + iG;
-  var bx = xb + iB;
-  //change these values to tint the colour
-  r = map(noise(rx, 1), 0, 1, 0, 255);
-  g = map(noise(gx, 1), 0, 1, 0, 255);
-  b = map(noise(bx, 1), 0, 1, 0, 255);
-
-  xr += speed;
-  xg += speed;
-  xb += speed;
-
-
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth -15, windowHeight - 200)
-}
-
-/*function mouseDragged(){
-    if(mouseButton === LEFT && counter % 1 == 0){
-        let newParticle = new particle(mouseX, mouseY, random(5), random(5))
-        particles.push(newParticle)
-    }
-}*/
-
-function analyse(){
-    analysisMode = true
-    planets.splice(0, planets.length)
-    particles.splice(0, particles.length)
-    background(255)
-    spawnParticles = false;
-    for(var i = 0; i < planetData.length; i++){
-      let newPlanet = new planet(planetData[i][0].x, planetData[i][0].y, planetData[i][1], true)
-      planets.push(newPlanet)
-      planets[i].colour = planetData[i][2]
-    }
-    particleWaitingList = []
-    for(var i = 0; i < particleData.length; i++){
-      let newParticle = new particle(particleData[i][0].x, particleData[i][0].y, particleData[i][1].x, particleData[i][1].y, true)
-      particleWaitingList.push(newParticle)
-    }
-}
-
-function keyPressed(){
-    if(keyCode == 78 && analysisMode){
-      background(255)
-      //Add succesful particles one at a time:
-      if(particleWaitingList.length > 3){
-      particles.push(particleWaitingList[0])
-      particles.push(particleWaitingList[1])
-      particles.push(particleWaitingList[2])
-      particleWaitingList.splice(0, 3)
-
-      //Add all succesful particles at once
-      // if(particleWaitingList.length > 1){
-      //  particles.push(particleWaitingList[0])
-      //  console.log(particles)
-      //  particleWaitingList.splice(0, 1)
-      // for(var i = 0; i < particleWaitingList.length; i++){
-      //   particles.push(particleWaitingList[i]);
-      // }
-    }else{
-      for(var i = 0; i < particleWaitingList.length; i++){
-        particles.push(particleWaitingList[i]);
-      }
-      console.log("Empty waiting list")
-    }
-  }else if(keyCode == 82){
-    analyse()
+    particles.push(new particle(random(-universeSizeX, universeSizeX), random(-universeSizeY, universeSizeY), random(-universeSizeZ, universeSizeZ), random(-5, 5), random(-5, 5), random(-5, 5)))
   }
 }
 
+function onWindowResize(){
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
-function particle(x, y, velX, velY, redrawn){
-  this.position = createVector(x, y)
-  this.velocity = createVector(velX, velY)
-  this.acceleration = createVector(0, 0)
+function particle(x, y, z, velX, velY, velZ, redrawn){
+  this.position = createVector(x, y, z)
+  this.velocity = createVector(velX, velY, velZ)
+  this.acceleration = createVector(0, 0, 0)
   this.maxSpeed = 10
   this.angle = 0
 
-  this.redrawn = redrawn
+  this.newLine = false;
 
-  if(!this.redrawn){
-  this.logged = false
-  this.data = []
-  this.data.push(this.position)
-  if(this.position.y < 100){
-    console.log("bug")
-  }
-  this.data.push(this.velocity)
-  }else{
-    this.logged = true
-  }
+  // this.redrawn = redrawn
+  //
+  // if(!this.redrawn){
+  // this.logged = false
+  // this.data = []
+  // this.data.push(this.position)
+  // if(this.position.y < 100){
+  //   console.log("bug")
+  // }
+  // this.data.push(this.velocity)
+  // }else{
+  //   this.logged = true
+  // }
 
   this.trail = []
-  this.randomColour = color(random(255), random(255), random(255))
+  this.randomColour = createVector(Math.floor(random(255)), Math.floor(random(255)), Math.floor(random(255)))
 
   this.lifetime = 0
 
@@ -190,13 +140,18 @@ function particle(x, y, velX, velY, redrawn){
   this.destroy = false
 
   this.update = function(){
-    var pos = createVector(this.position.x, this.position.y)
+    var pos = createVector(this.position.x, this.position.y, this.position.z)
     this.trail.push(pos)
     this.velocity.add(this.acceleration)
     this.velocity.mult(speedMultiplier)
     this.velocity.limit(this.maxSpeed)
 
     this.position.add(this.velocity)
+    if(this.velocity.mag() > 0){
+          this.newLine = true;
+        }else{
+          this.newLine = false;
+        }
     this.acceleration.mult(0)
     this.lifetime ++
   }
@@ -208,47 +163,74 @@ function particle(x, y, velX, velY, redrawn){
     this.acceleration.add(force)
   }
 
+  const generateColor = (forceMag = createVector(this.velocity.mag(), this.velocity.mag())) => {
+          forceMag.setMag(0.91);
+          console.log(forceMag);
+            const rgbArray = [
+                100 * Math.pow(forceMag.x + 0.2, 8),
+                60,
+                100 * Math.pow(forceMag.y + 0.2, 8)
+            ]
+            const [red, green, blue] = rgbArray.map(value => Math.round(value))
+            var particleColour = color (red, green, blue)
+            return `rgb(${red},${green},${blue})`
+            }
+
   this.display = function(){
-
-  //translate(offset)
-  if(this.trail.length > 1){
-  let index = this.trail.length - 1
-  if(this.redrawn){
-    //Perlin Noise Colour:
-    // let c = color(r, g, b)
-    // stroke(c)
-
-    stroke(this.randomColour)
-  }else{
-    stroke(128, 128, 128, 50)
-  }
-
-  line(this.position.x, this.position.y, this.trail[index].x, this.trail[index].y)
-
-  if(!this.redrawn){
-  if(this.lifetime < 600){
-    let c = color(128, 128, 128, 50)
-    stroke(c);
-  }
-  else if(this.lifetime > 600 && this.lifetime < 1200){
-    stroke(128, 128, 128, 100)
-  } else if(this.lifetime > 1200 && this.lifetime < 2000){
-    stroke(128, 128, 128, 200)
-  }else if(this.lifetime > 5000){
-    stroke(this.randomColour)
-  }
-  if(this.lifetime > 5000 && this.logged == false){
-    console.log("successful orbit")
-    particleData.push(this.data)
-    console.log(particleData)
-    this.logged = true;
-  }
-  line(this.position.x, this.position.y, this.trail[index].x, this.trail[index].y)
-}else{
-  stroke(this.randomColour)
+    if(this.destroy){
+      var selectedObject = scene.getObjectByName(objName);
+        scene.remove( selectedObject );
     }
-  }
-}
+    if(this.destroy == false && this.newLine){
+        if(this.trail.length > 1){
+        if(counter % 1 == 0){
+        let index = this.trail.length - 1;
+
+        var material = new THREE.LineBasicMaterial({color: color});
+
+        var geometry = new THREE.Geometry();
+        geometry.vertices.push(new THREE.Vector3(this.position.x, this.position.y, this.position.z));
+        geometry.vertices.push(new THREE.Vector3(this.trail[index].x, this.trail[index].y, this.trail[index].z));
+        this.line = new THREE.Line(geometry, material);
+
+        this.line.material.color = new THREE.Color(generateColor());
+        //this.line.material.needsUpdate = true;
+        scene.add(this.line);
+        }
+      }
+    }
+
+  // if(this.redrawn){
+  //   stroke(this.randomColour)
+  // }else{
+  //   stroke(128, 128, 128, 50)
+  // }
+
+
+
+//   if(!this.redrawn){
+//   if(this.lifetime < 600){
+//     let c = color(128, 128, 128, 50)
+//     stroke(c);
+//   }
+//   else if(this.lifetime > 600 && this.lifetime < 1200){
+//     stroke(128, 128, 128, 100)
+//   } else if(this.lifetime > 1200 && this.lifetime < 2000){
+//     stroke(128, 128, 128, 200)
+//   }else if(this.lifetime > 5000){
+//     stroke(this.randomColour)
+//   }
+//   if(this.lifetime > 5000 && this.logged == false){
+//     console.log("successful orbit")
+//     particleData.push(this.data)
+//     console.log(particleData)
+//     this.logged = true;
+//   }
+//   line(this.position.x, this.position.y, this.trail[index].x, this.trail[index].y)
+// }else{
+//   stroke(this.randomColour)
+//     }
+//   }
 
   this.edges = function(){
        if(this.position.x > width){
@@ -267,28 +249,5 @@ function particle(x, y, velX, velY, redrawn){
            this.hitedge = -2
        }
     }
+  }
 }
-
-// class Camera {
-//
-//   constructor() {
-//     this.pos = createVector(0, 0);
-//   }
-//
-//     draw() {
-//     //I used the mouse to move the camera
-//     //The mouse's position is always relative to the screen and not the camera's position
-//     //E.g. if the mouse is at 1000,1000 then the mouse's position does not add 1000,1000 to keep up with the camera
-//     //if (mouseX < 100) pos.x-=5;
-//     //else if (mouseX > width - 100) pos.x+=5;
-//     // if (mouseY < 100) pos.y-=5;
-//     //else if (mouseY > height - 100) pos.y+=5;
-//     //I noticed on the web the program struggles to find the mouse so I made it key pressed
-//     if (keyPressed) {
-//       if (key == 'w') this.pos.y -= 5;
-//       if (key == 's') this.pos.y += 5;
-//       if (key == 'a') this.pos.x -= 5;
-//       if (key == 'd') this.pos.x += 5;
-//     }
-//   }
-// }
