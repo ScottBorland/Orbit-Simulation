21fce923a73be66d9d912c8334fbd40f803b4a1b
ScottBorland <scottborland0@gmail.com>
2020-02-21 17:09:48 +0000

    maxspeed removed and visuals changed so that background is refreshed every frame and planets in orbit are displayed as small ellipses

--- sketch.js
+++ sketch.js
@@ -1,156 +1,121 @@
-var worldCamera
+
 var offset;
 
 var counter = 100;
 
 var speedMultiplier = 1
 var distanceScaler = 1000
-var planetGravityScaler = 1
+var sunGravityScaler = 1
 
-particles = []
 planets = []
+suns = []
 
+sunData = []
 planetData = []
-particleData = []
 
 var analysisMode = false
 
-var spawnParticles = true
-
-//perlin noise variables
-var xr = 0.0
-var xg = 100.0
-var xb = 2000.0
-
-//arbitrary values
-var iR = 1000
-var iB = 100
-var iG = 1
-
-//rate at which perlin noise function is looped through
-var speed = 0.02
+var spawnplanets = true
 
 function setup() {
   createCanvas(1920, 1080)
   background(255)
 
-  //worldCamera = new Camera()
+  // suns.push(new sun(400, 200, 10))
+  // suns.push(new sun(600, 200, 10))
+  // suns.push(new sun(500, 400, 10))
 
-  //this can be played around with, scale is between 1 and 4, fallout is from 0 to 1. (4, 1) gives a very washed out look. (2, 0.2) seems the best so far.
-  noiseDetail(2, 0.2)
-
-  // planets.push(new planet(400, 200, 10))
-  // planets.push(new planet(600, 200, 10))
-  // planets.push(new planet(500, 400, 10))
-
-  //particles.push(new particle(330, 200, 2, -7))
-  //particles.push(new particle(500, 100, 4, 6))
+  //planets.push(new planet(330, 200, 2, -7))
+  //planets.push(new planet(500, 100, 4, 6))
 
   for(var i = 0; i < 2; i++){
-    planets.push(new planet(random(200, windowWidth - 200), random(200, windowHeight-200), random(20), false))
+    suns.push(new sun(random(200, windowWidth - 200), random(200, windowHeight-200), random(20), false))
   }
   offset = createVector(0, 0)
 }
 
 function draw() {
+  background(255);
+  // if(!analysisMode){
+  //   //translate(width / 2, height / 2);
+  //   scale(0.8)
+  // }else{
+  //   scale(0.8)
+  // }
 
-  if(!analysisMode){
-    //translate(width / 2, height / 2);
-    scale(0.8)
-  }else{
-    scale(0.8)
-  }
-
-  for(var i = 0; i < particles.length; i++){
-    if(particles[i].destroy){
-      particles.splice(i, 1)
+  for(var i = 0; i < planets.length; i++){
+    if(planets[i].destroy){
+      planets.splice(i, 1)
     }
   }
 
-  for(var i = 0; i < particles.length; i++){
-    particles[i].update()
-    //particles[i].edges()
-    particles[i].display()
+  for(var i = 0; i < planets.length; i++){
+    planets[i].update()
+    //planets[i].edges()
+    //planets[i].display()
+    planets[i].showAsCircle();
   }
 
-  for(var i = 0; i < planets.length; i++){
-    planets[i].display()
-    planets[i].applyGravity()
+  for(var i = 0; i < suns.length; i++){
+    suns[i].display()
+    suns[i].applyGravity()
   }
   counter++;
   if(counter > 100){
       counter = 0
   }
-  if(spawnParticles && counter % 3 == 0){
-    particles.push(new particle(random(400, windowWidth -400), random(400, windowHeight -400), random(5), random(5), false))
-  }
+   if(spawnplanets && counter % 20 == 0 && planets.length < 7){
+     planets.push(new planet(random(400, windowWidth -400), random(400, windowHeight -400), random(5), random(5), false))
+   }
+}
 
-  //perlin noise
-  var rx = xr + iR;
-  var gx = xg + iG;
-  var bx = xb + iB;
-  //change these values to tint the colour
-  r = map(noise(rx, 1), 0, 1, 0, 255);
-  g = map(noise(gx, 1), 0, 1, 0, 255);
-  b = map(noise(bx, 1), 0, 1, 0, 255);
-
-  xr += speed;
-  xg += speed;
-  xb += speed;
-
-
+function mousePressed(){
+  planets.push(new planet(mouseX, mouseY, random(10), random(10), false))
 }
 
 function windowResized() {
   resizeCanvas(windowWidth -15, windowHeight - 200)
 }
 
-/*function mouseDragged(){
-    if(mouseButton === LEFT && counter % 1 == 0){
-        let newParticle = new particle(mouseX, mouseY, random(5), random(5))
-        particles.push(newParticle)
-    }
-}*/
-
 function analyse(){
     analysisMode = true
+    suns.splice(0, suns.length)
     planets.splice(0, planets.length)
-    particles.splice(0, particles.length)
     background(255)
-    spawnParticles = false;
-    for(var i = 0; i < planetData.length; i++){
-      let newPlanet = new planet(planetData[i][0].x, planetData[i][0].y, planetData[i][1], true)
-      planets.push(newPlanet)
-      planets[i].colour = planetData[i][2]
+    spawnplanets = false;
+    for(var i = 0; i < sunData.length; i++){
+      let newsun = new sun(sunData[i][0].x, sunData[i][0].y, sunData[i][1], true)
+      suns.push(newsun)
+      suns[i].colour = sunData[i][2]
     }
-    particleWaitingList = []
-    for(var i = 0; i < particleData.length; i++){
-      let newParticle = new particle(particleData[i][0].x, particleData[i][0].y, particleData[i][1].x, particleData[i][1].y, true)
-      particleWaitingList.push(newParticle)
+    planetWaitingList = []
+    for(var i = 0; i < planetData.length; i++){
+      let newplanet = new planet(planetData[i][0].x, planetData[i][0].y, planetData[i][1].x, planetData[i][1].y, true)
+      planetWaitingList.push(newplanet)
     }
 }
 
 function keyPressed(){
     if(keyCode == 78 && analysisMode){
       background(255)
-      //Add succesful particles one at a time:
-      if(particleWaitingList.length > 3){
-      particles.push(particleWaitingList[0])
-      particles.push(particleWaitingList[1])
-      particles.push(particleWaitingList[2])
-      particleWaitingList.splice(0, 3)
+      //Add succesful planets one at a time:
+      if(planetWaitingList.length > 3){
+      planets.push(planetWaitingList[0])
+      planets.push(planetWaitingList[1])
+      planets.push(planetWaitingList[2])
+      planetWaitingList.splice(0, 3)
 
-      //Add all succesful particles at once
-      // if(particleWaitingList.length > 1){
-      //  particles.push(particleWaitingList[0])
-      //  console.log(particles)
-      //  particleWaitingList.splice(0, 1)
-      // for(var i = 0; i < particleWaitingList.length; i++){
-      //   particles.push(particleWaitingList[i]);
+      //Add all succesful planets at once
+      // if(planetWaitingList.length > 1){
+      //  planets.push(planetWaitingList[0])
+      //  console.log(planets)
+      //  planetWaitingList.splice(0, 1)
+      // for(var i = 0; i < planetWaitingList.length; i++){
+      //   planets.push(planetWaitingList[i]);
       // }
     }else{
-      for(var i = 0; i < particleWaitingList.length; i++){
-        particles.push(particleWaitingList[i]);
+      for(var i = 0; i < planetWaitingList.length; i++){
+        planets.push(planetWaitingList[i]);
       }
       console.log("Empty waiting list")
     }
@@ -160,12 +125,22 @@ function keyPressed(){
 }
 
 
-function particle(x, y, velX, velY, redrawn){
+function planet(x, y, velX, velY, redrawn){
   this.position = createVector(x, y)
   this.velocity = createVector(velX, velY)
   this.acceleration = createVector(0, 0)
   this.maxSpeed = 10
   this.angle = 0
+
+  const randomColour = () => {
+          var red = Math.floor(random(255))
+          var green = Math.floor(random(255))
+          var blue = Math.floor(random(255))
+          var planetColour = color (red, green, blue)
+          return planetColour
+  }
+
+  this.colour = randomColour();
 
   this.redrawn = redrawn
 
@@ -193,8 +168,8 @@ function particle(x, y, velX, velY, redrawn){
     var pos = createVector(this.position.x, this.position.y)
     this.trail.push(pos)
     this.velocity.add(this.acceleration)
-    this.velocity.mult(speedMultiplier)
-    this.velocity.limit(this.maxSpeed)
+    //this.velocity.mult(speedMultiplier)
+    //this.velocity.limit(this.maxSpeed)
 
     this.position.add(this.velocity)
     this.acceleration.mult(0)
@@ -208,16 +183,36 @@ function particle(x, y, velX, velY, redrawn){
     this.acceleration.add(force)
   }
 
+  this.showAsCircle = function (){
+    stroke(0);
+    fill(this.colour);
+    ellipse(this.position.x, this.position.y, 12)
+    noFill();
+    stroke(this.colour)
+    beginShape();
+    if(this.trail.length > 2000){
+      this.trail.splice(0, 5)
+    }
+    for(var i = 0; i < this.trail.length; i++){
+      var pos = this.trail[i];
+      vertex(pos.x, pos.y);
+      }
+      endShape();
+
+      if(this.lifetime > 2000 && this.logged == false){
+        console.log("successful orbit")
+        planetData.push(this.data)
+        console.log(planetData)
+        this.logged = true;
+      }
+  }
+
   this.display = function(){
 
   //translate(offset)
   if(this.trail.length > 1){
   let index = this.trail.length - 1
   if(this.redrawn){
-    //Perlin Noise Colour:
-    // let c = color(r, g, b)
-    // stroke(c)
-
     stroke(this.randomColour)
   }else{
     stroke(128, 128, 128, 50)
@@ -239,8 +234,8 @@ function particle(x, y, velX, velY, redrawn){
   }
   if(this.lifetime > 5000 && this.logged == false){
     console.log("successful orbit")
-    particleData.push(this.data)
-    console.log(particleData)
+    planetData.push(this.data)
+    console.log(planetData)
     this.logged = true;
   }
   line(this.position.x, this.position.y, this.trail[index].x, this.trail[index].y)
@@ -268,27 +263,3 @@ function particle(x, y, velX, velY, redrawn){
        }
     }
 }
-
-// class Camera {
-//
-//   constructor() {
-//     this.pos = createVector(0, 0);
-//   }
-//
-//     draw() {
-//     //I used the mouse to move the camera
-//     //The mouse's position is always relative to the screen and not the camera's position
-//     //E.g. if the mouse is at 1000,1000 then the mouse's position does not add 1000,1000 to keep up with the camera
-//     //if (mouseX < 100) pos.x-=5;
-//     //else if (mouseX > width - 100) pos.x+=5;
-//     // if (mouseY < 100) pos.y-=5;
-//     //else if (mouseY > height - 100) pos.y+=5;
-//     //I noticed on the web the program struggles to find the mouse so I made it key pressed
-//     if (keyPressed) {
-//       if (key == 'w') this.pos.y -= 5;
-//       if (key == 's') this.pos.y += 5;
-//       if (key == 'a') this.pos.x -= 5;
-//       if (key == 'd') this.pos.x += 5;
-//     }
-//   }
-// }
--- sun.js
+++ sun.js
@@ -1,17 +1,18 @@
-var strengthToSizeScaler = 3;
+var strengthToSizeScaler = 6;
 
-function planet(x, y, strength, redrawn){
+function sun(x, y, strength, redrawn){
   this.position = createVector(x, y)
   this.strength = strength
 
   this.redrawn = redrawn
   if(!this.redrawn){
-  this.colour = color(random(255), random(255), random(255))
+  //this.colour = color(random(255), random(255), random(255))
+  this.colour = color(224, 168, 1)
   this.data = []
   this.data.push(this.position)
   this.data.push(this.strength)
   this.data.push(this.colour)
-  planetData.push(this.data)
+  sunData.push(this.data)
 }
 
   this.display = function(){
@@ -21,17 +22,17 @@ function planet(x, y, strength, redrawn){
     ellipse(this.position.x, this.position.y, this.strength * strengthToSizeScaler)
   }
   this.applyGravity = function (){
-    for(var i = 0; i < particles.length; i++){
-      var dif = p5.Vector.sub(this.position, particles[i].position)
+    for(var i = 0; i < planets.length; i++){
+      var dif = p5.Vector.sub(this.position, planets[i].position)
       var dist = dif.mag()
       if(dif.mag() < 20){
-        particles[i].applyForce(createVector(1000, 1000))
+        planets[i].applyForce(createVector(1000, 1000))
       }else{
 
-      var mag = distanceScaler * planetGravityScaler / (dist * dist)
+      var mag = distanceScaler * sunGravityScaler / (dist * dist)
 
       dif.setMag(mag)
-      particles[i].applyForce(dif)
+      planets[i].applyForce(dif)
     }
     }
   }
